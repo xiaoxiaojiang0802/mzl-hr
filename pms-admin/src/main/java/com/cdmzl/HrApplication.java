d463c0e979109bda875167f6bcc63c7e2df1df1a
xiaojiang <1296581679@qq.com>
2023-10-07 20:51:23 +0800

    修改代码相关注释

--- pms-admin/src/main/java/com/cdmzl/HrApplication.java
+++ pms-admin/src/main/java/com/cdmzl/HrApplication.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * 启动程序
  *
- * @author jiangjin
+ * @author muzi
  */
 @SpringBootApplication
 @EnableTransactionManagement
